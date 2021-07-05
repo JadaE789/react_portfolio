3a308dd5e90b13c9fb14690f55a2a87d52e30b3f
Jada Desormeaux <JadaE789@gmail.com>
2021-07-05 14:12:32 -0400

    portfolio site and resume file

---
+++ src/components/PortfolioSite.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import { pageComponents } from './pages';
+
+export default function PortfolioSite() {
+  const pages = ['About', 'Portfolio', 'Contact', 'Resume'];
+
+  return (
+    <div className='flex flex-col min-h-screen relative overflow-hidden'>
+      <BrowserRouter>
+        <Header pages={pages} />
+        <main className='flex-grow flex'>
+          <Switch>
+            {/*Default to About page*/}
+            <Route path='/' exact component={pageComponents['About']} />
+            {pages.map((pageName, index) => (
+              <Route
+                key={index}
+                path={`/${pageName.toLowerCase()}`}
+                component={pageComponents[pageName]}
+              />
+            ))}
+            <Route render={() => <Redirect to='/' />} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
