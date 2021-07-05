159e79dc7bdae2902499ff6d8ce5b52b4525ec1f
Jada Desormeaux <JadaE789@gmail.com>
2021-07-05 14:42:47 -0400

    index, portfolio, and resume

---
+++ src/components/pages/index.js
@@ -0,0 +1,11 @@
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+
+export const pageComponents = {
+  About,
+  Portfolio,
+  Contact,
+  Resume,
+};
