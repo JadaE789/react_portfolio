d2c66d1787e13542ade1ea6dff0526489c0b0508
Jada Desormeaux <JadaE789@gmail.com>
2021-07-05 21:11:24 -0400

    about me info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,19 +6,11 @@ export default function About() {
       {/* md:self-center */}
       <div className='flex flex-col justify-center items-center md:w-1/2 p-6'>
         <h3 className=''> About </h3>
-        <p className=''>
-            In December 2019 I received my Associates degree in Information Technology. 
-            While I was in school I took Intro to Technology, Programming Logic 
-            and Design, Microsoft Access and Excel, A+ Hardware, Networking, and Project Management. 
-            I got a lot of experience and knowledge in almost all 
-            fields of IT. I took coding in high school for 
-            two years and I really did enjoy doing that because there's
-            a lot of room for creativity and allows my work to really show/reflect who I am.
-        </p>
-        <p className=''>
-          I'm looking forward to solving challenging problems in the technology
-          sector, all while feeding my passion for learning and understanding
-          how products and services are designed, built, and maintained.
+        <p className='aboutMe'>
+        I am a web developer that has skills in both the front-end of web applications and the back-end. 
+        I use my design skills to make applications look beautiful and I am also able to work on the technically challenging aspects of the application too. 
+        Bringing a keen eye for details, strong ability to implement mock-ups, and understanding of great user experience design, 
+        I'm looking forward to putting my abilities to work as part of a fast-paced, quality-driven team to improve web experiences. 
         </p>
       </div>
       <div className='bg-landing bg-cover bg-center bg-no-repeat h-full md:w-1/2'></div>
