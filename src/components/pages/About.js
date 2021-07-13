64e118ebe1176cf1e5fc414f8de6ede45e68187d
Jada Desormeaux <JadaE789@gmail.com>
2021-07-12 20:30:57 -0400

    about page and footer

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section className='flex flex-col md:flex-row'>
       {/* md:self-center */}
       <div className='flex flex-col justify-center items-center md:w-1/2 p-6'>
-        <h3 className=''> About </h3>
+        <h3 className='aboutHeader'> About </h3>
         <p className='aboutMe'>
         I am a web developer that has skills in both the front-end of web applications and the back-end. 
         I use my design skills to make applications look beautiful and I am also able to work on the technically challenging aspects of the application too. 
